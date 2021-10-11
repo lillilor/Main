4247aa786c8d2fa8667f272d86578e57dddc1f74
lillilor <lorenzolilli84@gmail.com>
2021-10-11 17:28:54 +0200

    About box text formattig

--- WindowsFormsApplication4/WindowsFormsApplication4/LILAboutBox.cs
+++ WindowsFormsApplication4/WindowsFormsApplication4/LILAboutBox.cs
@@ -14,7 +14,7 @@ namespace LILAutoClicker
             labelVersion.Text = $"Version {AssemblyVersion}";
             labelCopyright.Text = AssemblyCopyright;
             labelCompanyName.Text = "lillilor";
-            textBoxDescription.Text = "This program is free software; you can redistribute it and/or"+'\n'+"modify it under the terms of the GNU General Public License"+'\n'+"as published by the Free Software Foundation; either version 2\nof the License, or(at your option) any later version."+'\n'+'\n'+ "This program is distributed in the hope that it will be useful,"+'\n'+"but WITHOUT ANY WARRANTY; without even the implied warranty of" + '\n' + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the" + '\n' + "GNU General Public License for more details.";
+            textBoxDescription.Text = "This program is free software; you can redistribute it and/or "+'\n'+"modify it under the terms of the GNU General Public License "+'\n'+"as published by the Free Software Foundation; either version 2 "+'\n'+"of the License, or(at your option) any later version."+'\n'+'\n'+ "This program is distributed in the hope that it will be useful,"+'\n'+"but WITHOUT ANY WARRANTY; without even the implied warranty of " + '\n' + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the " + '\n' + "GNU General Public License for more details.";
         }
 
         #region Assembly Attribute Accessors
